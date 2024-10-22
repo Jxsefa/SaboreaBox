503a8639d2c263b45dc88cdf78c2f4894f6f4751
Josefa <josefa.castillo25@gmail.com>
2024-10-22 20:36:12 -0300

    add rutas y handelbars

--- app.js
+++ app.js
@@ -1,25 +1,60 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
-const path = require('path');
+const { engine } = require('express-handlebars');
 
 const app = express();
 const PORT = 3000;
 
-// Configuración de Handlebars
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
+const http = require("http");
+const { neon } = require("@neondatabase/serverless");
 
-// Middleware para manejar datos del formulario
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+//Base de Datos
+const sql = neon(process.env.DATABASE_URL);
 
-// Ruta principal
-app.get('/', (req, res) => {
-    res.render('home'); // Renderiza la vista 'home.handlebars'
+const requestHandler = async (req, res) => {
+    const result = await sql`SELECT version()`;
+    const { version } = result[0];
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(version);
+};
+
+http.createServer(requestHandler).listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
 });
 
-// Escucha del servidor
+// Configurar Handlebars como motor de vistas
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
+
+// Middleware para servir archivos estáticos
+app.use(express.static('./'));
+require("dotenv").config();
+
+// Rutas
+app.get('/', (req, res) => {
+    res.render('home', { title: 'Inicio' });
+});
+app.get('/admin', (req, res) => {
+    res.render('admin', { title: 'Administración' });
+});
+app.get('/cart', (req, res) => {
+    res.render('cart', { title: 'Carro compras' });
+});
+app.get('/login', (req, res) => {
+    res.render('login', { title: 'Inicio secion' });
+});
+app.get('/products', (req, res) => {
+    res.render('products', { title: 'Productos' });
+});
+app.get('/login/register', (req, res) => {
+    res.render('register', { title: 'Registro' });
+});
+app.get('/user', (req, res) => {
+    res.render('user', { title: 'Usuario' });
+});
+
+// Inicia el servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
